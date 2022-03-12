cd50bfa332a3a8117903b6e4dfb097ed51051858
Daniel Gomes <daniel0x7cc@gmail.com>
2022-03-12 01:52:03 -0300

    - feat: added snackbar with retry to handle request error case

--- movies/src/main/java/com/devlabs/movies/MoviesListActivity.kt
+++ movies/src/main/java/com/devlabs/movies/MoviesListActivity.kt
@@ -3,18 +3,24 @@ package com.devlabs.movies
 import android.os.Bundle
 import android.widget.ProgressBar
 import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.devlabs.core.extension.gone
 import com.devlabs.core.extension.visible
 import com.devlabs.domain.entity.Movie
 import com.devlabs.domain.entity.ResultWrapper
+import com.google.android.material.snackbar.Snackbar
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class MoviesListActivity : AppCompatActivity() {
     private val viewModel by viewModel<MoviesListViewModel>()
     private val moviesAdapter: MoviesListAdapter by lazy {
         MoviesListAdapter(this)
+    }
+
+    private val containerRoot: ConstraintLayout by lazy {
+        findViewById(R.id.activity_movies_root)
     }
 
     private val progressLoading: ProgressBar by lazy {
@@ -39,7 +45,7 @@ class MoviesListActivity : AppCompatActivity() {
     private fun handleResult(resultState: ResultWrapper<List<Movie>>?) {
         when (resultState) {
             ResultWrapper.Empty -> {
-
+                displayRetrySnackBar()
             }
             ResultWrapper.Loading -> {
                 progressLoading.visible()
@@ -50,9 +56,16 @@ class MoviesListActivity : AppCompatActivity() {
                 setupMoviesList(resultState.value)
             }
             is ResultWrapper.GenericError -> {
-
+                displayRetrySnackBar()
             }
         }
+    }
+
+    private fun displayRetrySnackBar() {
+        progressLoading.gone()
+        Snackbar.make(containerRoot, getString(R.string.error_message_data), Snackbar.LENGTH_INDEFINITE).setAction(getString(R.string.retry)) {
+            viewModel.getMoviesList()
+        }.show()
     }
 
     private fun setupMoviesList(list: List<Movie>) {
